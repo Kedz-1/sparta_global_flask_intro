e487380919a992670af184e4ca2fbcd16e749893
kedz-1 <khadar7@hotmail.co.uk>
2026-08-13 14:47:35 +0100

    file for flask created along with json and csv data

---
+++ src/basic_app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import json
+import csv
+import pandas as pd
+
+
+app = Flask(__name__)
+
+
+# with open('one_piece_characters.json', 'r') as file:
+#     characters = json.load(file)
+
+
+characters = pd.read_csv('one_piece_characters.csv') 
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/characters', methods=['GET'])
+def send_json():
+
+    return jsonify(characters.to_dict())
+
+if __name__ == '__main__':
+    app.run(debug=True)
